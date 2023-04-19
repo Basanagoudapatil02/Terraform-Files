0a4573751b60ba8fbd2d6cf875a87da535f37226
Basanagouda Patil <63364609+Basanagoudapatil02@users.noreply.github.com>
2023-04-19 18:34:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ variable "Secretaccesskey" {}
 provider "aws" {
 region= "us-east-1"
 access_key = var.accesskey
-secret_key = var.secretkey
+secret_key = var.Secretaccesskey
 }
 
 resource "aws_vpc" "customvpc" {
